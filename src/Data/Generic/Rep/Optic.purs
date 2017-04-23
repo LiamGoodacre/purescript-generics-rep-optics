4179dfaa2c5cf63d98c19ca1c019e2f2c66bb903
Liam Goodacre <goodacre.liam@gmail.com>
2017-04-23 15:31:23 +0100

    Add argument lookup

--- src/Data/Generic/Rep/Optic.purs
+++ src/Data/Generic/Rep/Optic.purs
@@ -5,25 +5,50 @@ module Data.Generic.Rep.Optic
   , gCtor
   , class CtorSum
   , gCtorSum
+  , ctorArgs
+  , class Args
+  , gArgs
+  , Z
+  , S
+  , class NatKey
+  , class ArgsProd
+  , gArgsProd
+  , class ArgsRec
+  , gArgsRec
+  , class ArgsRecCheck
+  , gArgsRecCheck
+  , _0
+  , _1
+  , _2
+  , _3
+  , _4
+  , _5
+  , _6
   ) where
 
+import Data.Generic.Rep
 import Prelude ((<<<))
-import Data.Generic.Rep (class Generic, from, to, Sum(..), Constructor(..), Argument(..))
-import Data.Lens (Iso', iso, Prism', prism')
-import Type.Data.Symbol (SProxy, class CompareSymbol)
+import Data.Lens (Iso', iso, Prism', prism', Lens', lens)
+import Type.Data.Symbol (SProxy(..), class CompareSymbol)
 import Type.Data.Ordering (OProxy(..), EQ, LT, GT, kind Ordering)
+import Type.Proxy (Proxy(..))
 import Data.Maybe (Maybe(..))
 import Data.Profunctor (class Profunctor)
+import Data.Profunctor.Strong (class Strong)
 import Data.Profunctor.Choice (class Choice)
 
 -- | Isomorphism between data and it's generic representation
 rep :: forall s a. Generic s a => Iso' s a
 rep = iso from to
 
--- | Iso over the Arg of a Ctor
-gArg :: forall c a. Iso' (Constructor c (Argument a)) a
-gArg = iso alpha (Constructor <<< Argument) where
-  alpha (Constructor (Argument a)) = a
+iCtor :: forall c a. Iso' (Constructor c a) a
+iCtor = iso (\(Constructor a) -> a) Constructor
+
+iArg :: forall a. Iso' (Argument a) a
+iArg = iso (\(Argument a) -> a) Argument
+
+iField :: forall fd a. Iso' (Field fd a) a
+iField = iso (\(Field a) -> a) Field
 
 -- | Prism into the left of a Sum
 gInl :: forall l r. Prism' (Sum l r) l
@@ -37,37 +62,43 @@ gInr = prism' Inr alpha where
   alpha (Inr r) = Just r
   alpha _ = Nothing
 
--- | Prism into a ctor of a Sum
+gProl :: forall l r. Lens' (Product l r) l
+gProl = lens (\(Product l r) -> l) (\(Product _ r) l -> Product l r)
+
+gPror :: forall l r. Lens' (Product l r) r
+gPror = lens (\(Product l r) -> r) (\(Product l _) r -> Product l r)
+
+-- | Optic into a ctor of a Sum
 class Ctor (ct :: Symbol)
-           (gen :: Type)
+           (rep :: Type)
            (out :: Type)
            (p :: Type -> Type -> Type)
-           | ct gen -> out p where
+           | ct rep -> out p where
   gCtor :: SProxy ct ->
            p out out ->
-           p gen gen
+           p rep rep
 
 -- | Rep only has a ctor
 instance ctorCtor
   :: Profunctor p
-  => Ctor ct (Constructor ct (Argument arg)) arg p where
-  gCtor _ = gArg
+  => Ctor ct (Constructor ct arg) arg p where
+  gCtor _ = iCtor
 
 -- | Dispatch on equal symbols
 class CtorSum (ord :: Ordering)
               (ct :: Symbol)
-              (gen :: Type)
+              (rep :: Type)
               (out :: Type)
               (p :: Type -> Type -> Type)
-              | ord -> ct gen out p where
+              | ord -> ct rep out p where
   gCtorSum :: OProxy ord ->
               SProxy ct ->
               p out out ->
-              p gen gen
+              p rep rep
 instance ctorSumEQ
   :: Choice p
-  => CtorSum EQ ct (Sum (Constructor ct (Argument arg)) rest) arg p where
-  gCtorSum _ _ = gInl <<< gArg
+  => CtorSum EQ ct (Sum (Constructor ct arg) rest) arg p where
+  gCtorSum _ _ = gInl <<< iCtor
 instance ctorSumLT
   :: ( Choice p
      , Ctor ct rest out p )
@@ -83,9 +114,9 @@ instance ctorSumGT
 instance ctorSum
   :: ( Choice p
      , CompareSymbol ct cur ord
-     , CtorSum ord ct (Sum (Constructor cur (Argument arg)) rest) out p
+     , CtorSum ord ct (Sum (Constructor cur arg) rest) out p
      )
-  => Ctor ct (Sum (Constructor cur (Argument arg)) rest) out p where
+  => Ctor ct (Sum (Constructor cur arg) rest) out p where
   gCtor ct = gCtorSum (OProxy :: OProxy ord) ct
 
 -- | Optic into a constructor of a data type with a generics-rep instance.
@@ -110,9 +141,141 @@ instance ctorSum
 ctor :: forall d g c a p.
   Profunctor p =>
   Generic d g =>
-  Ctor c g a p =>
+  Ctor c g (Argument a) p =>
   SProxy c ->
   p a a ->
   p d d
-ctor c = rep <<< gCtor c
+ctor c = rep <<< gCtor c <<< iArg
+
+class Args (fd :: Symbol)
+           (rep :: Type)
+           (out :: Type)
+           (p :: Type -> Type -> Type)
+           | fd rep -> out p where
+  gArgs :: SProxy fd ->
+           p out out ->
+           p rep rep
+instance argsProd
+  :: ( NatKey n fd
+     , ArgsProd n (Product l r) out p )
+  => Args fd (Product l r) out p where
+  gArgs fd = gArgsProd (Proxy :: Proxy n)
+instance argsRec
+  :: ( Profunctor p
+     , ArgsRec fd r o p )
+  => Args fd (Rec r) o p where
+  gArgs fd = iso (\(Rec r) -> r) Rec <<< gArgsRec fd
+
+data Z = Z
+data S n = S n
+
+class NatKey (n :: Type) (s :: Symbol) | n -> s, s -> n
+instance natKey0 :: NatKey Z "value0"
+instance natKey1 :: NatKey (S Z) "value1"
+instance natKey2 :: NatKey (S (S Z)) "value2"
+instance natKey3 :: NatKey (S (S (S Z))) "value3"
+instance natKey4 :: NatKey (S (S (S (S Z)))) "value4"
+instance natKey5 :: NatKey (S (S (S (S (S Z))))) "value5"
+instance natKey6 :: NatKey (S (S (S (S (S (S Z)))))) "value6"
+
+class ArgsProd (count :: Type)
+               (rep :: Type)
+               (out :: Type)
+               (p :: Type -> Type -> Type)
+               | count rep -> out p where
+  gArgsProd :: Proxy count ->
+               p out out ->
+               p rep rep
+instance argsProdBaseA
+  :: Profunctor p
+  => ArgsProd Z (Argument arg) arg p where
+  gArgsProd z = iArg
+instance argsProdBaseP
+  :: Strong p
+  => ArgsProd Z (Product (Argument arg) r) arg p where
+  gArgsProd z = gProl <<< iArg
+instance argsProdStep
+  :: ( Strong p
+     , ArgsProd n r out p )
+  => ArgsProd (S n) (Product l r) out p where
+  gArgsProd _ = gPror <<< gArgsProd (Proxy :: Proxy n)
+
+class ArgsRec (fd :: Symbol)
+              (rep :: Type)
+              (out :: Type)
+              (p :: Type -> Type -> Type)
+              | rep -> out p where
+  gArgsRec :: SProxy fd ->
+              p out out ->
+              p rep rep
+instance argsRecBase
+  :: Profunctor p
+  => ArgsRec fd (Field fd t) t p where
+  gArgsRec _ = iField
+instance argsRecStep
+  :: ( Strong p
+     , CompareSymbol fd lfd ord
+     , ArgsRecCheck ord fd (Product (Field lfd lty) r) lfd lty r out p )
+  => ArgsRec fd (Product (Field lfd lty) r) out p where
+  gArgsRec = gArgsRecCheck (OProxy :: OProxy ord)
+
+class ArgsRecCheck (ord :: Ordering)
+                   (fd :: Symbol)
+                   (rep :: Type)
+                   (lfd :: Symbol)
+                   (lty :: Type)
+                   (right :: Type)
+                   (out :: Type)
+                   (p :: Type -> Type -> Type)
+                   | ord fd -> rep lfd lty right out p where
+  gArgsRecCheck :: OProxy ord ->
+                   SProxy fd ->
+                   p out out ->
+                   p rep rep
+instance argsRecCheckEQ
+  :: Strong p
+  => ArgsRecCheck EQ fd (Product (Field lfd lty) right) lfd lty right lty p where
+  gArgsRecCheck _ fd = gProl <<< iField
+instance argsRecCheckLT
+  :: ( Strong p
+     , ArgsRec fd right out p )
+  => ArgsRecCheck LT fd (Product left right) lfd lty right out p where
+  gArgsRecCheck _ fd = gPror <<< gArgsRec fd
+instance argsRecCheckGT
+  :: ( Strong p
+     , ArgsRec fd right out p )
+  => ArgsRecCheck GT fd (Product left right) lfd lty right out p where
+  gArgsRecCheck _ fd = gPror <<< gArgsRec fd
+
+ctorArgs :: forall s g c fd args a p.
+  Profunctor p =>
+  Generic s g =>
+  Ctor c g args p =>
+  Args fd args a p =>
+  SProxy c ->
+  SProxy fd ->
+  p a a ->
+  p s s
+ctorArgs c fd = rep <<< gCtor c <<< gArgs fd
+
+_0 :: SProxy "value0"
+_0 = SProxy
+
+_1 :: SProxy "value1"
+_1 = SProxy
+
+_2 :: SProxy "value2"
+_2 = SProxy
+
+_3 :: SProxy "value3"
+_3 = SProxy
+
+_4 :: SProxy "value4"
+_4 = SProxy
+
+_5 :: SProxy "value5"
+_5 = SProxy
+
+_6 :: SProxy "value6"
+_6 = SProxy
 
--- test/Main.purs
+++ test/Main.purs
@@ -4,9 +4,9 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Optic (ctor)
+import Data.Generic.Rep.Optic (ctor, ctorArgs, _0, _1)
 import Type.Data.Symbol (SProxy(..))
-import Data.Lens (over, view, preview)
+import Data.Lens (over, view, preview, Lens')
 import Data.Maybe (Maybe(..))
 import Test.Assert (ASSERT, assert)
 
@@ -15,11 +15,45 @@ derive instance genericI :: Generic I _
 derive instance eqI :: Eq I
 _I = SProxy :: SProxy "I"
 
-data E = L Int | R Boolean
+data E = A Int | B Boolean
 derive instance genericE :: Generic E _
 derive instance eqE :: Eq E
-_L = SProxy :: SProxy "L"
+_A = SProxy :: SProxy "A"
+_B = SProxy :: SProxy "B"
+
+data P = P Int String
+derive instance genericP :: Generic P _
+_P = SProxy :: SProxy "P"
+_P_0 :: Lens' P Int
+_P_0 = ctorArgs _P _0
+_P_1 :: Lens' P String
+_P_1 = ctorArgs _P _1
+
+data R = R { a :: Int, b :: String }
+derive instance genericR :: Generic R _
 _R = SProxy :: SProxy "R"
+_a = SProxy :: SProxy "a"
+_b = SProxy :: SProxy "b"
+_R_a :: Lens' R Int
+_R_a = ctorArgs _R _a
+_R_b :: Lens' R String
+_R_b = ctorArgs _R _b
+
+data G t = G t t
+derive instance genericG :: Generic (G t) _
+_G = SProxy :: SProxy "G"
+_G_0 :: forall t. Lens' (G t) t
+_G_0 = ctorArgs _G _0
+_G_1 :: forall t. Lens' (G t) t
+_G_1 = ctorArgs _G _1
+
+data N = N (G Int) (G String)
+derive instance genericN :: Generic N _
+_N = SProxy :: SProxy "N"
+_N_0 :: Lens' N (G Int)
+_N_0 = ctorArgs _N _0
+_N_1 :: Lens' N (G String)
+_N_1 = ctorArgs _N _1
 
 main :: Eff (console :: CONSOLE, assert :: ASSERT) Unit
 main = do
@@ -27,18 +61,37 @@ main = do
     assert $ I "Hi!" == over (ctor _I) (_ <> "!") (I "Hi")
     assert $ "Hi" == view (ctor _I) (I "Hi")
 
-  log "E _L" *> do
-    let f = over (ctor _L) (10 * _)
-    assert $ L 420  == f (L 42)
-    assert $ R true == f (R true)
-    assert $ Just 42 == preview (ctor _L) (L 42)
-    assert $ Nothing == preview (ctor _L) (R true)
+  log "E _A" *> do
+    let f = over (ctor _A) (10 * _)
+    assert $ A 420  == f (A 42)
+    assert $ B true == f (B true)
+    assert $ Just 42 == preview (ctor _A) (A 42)
+    assert $ Nothing == preview (ctor _A) (B true)
 
-  log "E _R" *> do
-    let f = over (ctor _R) not
-    assert $ L 42    == f (L 42)
-    assert $ R false == f (R true)
-    assert $ Nothing   == preview (ctor _R) (L 42)
-    assert $ Just true == preview (ctor _R) (R true)
+  log "E _B" *> do
+    let f = over (ctor _B) not
+    assert $ A 42    == f (A 42)
+    assert $ B false == f (B true)
+    assert $ Nothing   == preview (ctor _B) (A 42)
+    assert $ Just true == preview (ctor _B) (B true)
+
+  log "P" *> do
+    assert $ 42   == view _P_0 (P 42 "Hi")
+    assert $ "Hi" == view _P_1 (P 42 "Hi")
+
+  log "R" *> do
+    assert $ 42   == view _R_a (R {a: 42, b: "Hi"})
+    assert $ "Hi" == view _R_b (R {a: 42, b: "Hi"})
+
+  log "G" *> do
+    assert $ "A" == view _G_0 (G "A" "B")
+    assert $ "B" == view _G_1 (G "A" "B")
+
+  log "N" *> do
+    let n = N (G 1 2) (G "A" "B")
+    assert $ 1   == view (_N_0 <<< _G_0) n
+    assert $ 2   == view (_N_0 <<< _G_1) n
+    assert $ "A" == view (_N_1 <<< _G_0) n
+    assert $ "B" == view (_N_1 <<< _G_1) n
 
   log "All done!"
